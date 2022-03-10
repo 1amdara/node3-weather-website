91ed1081798b243164b318c6c123b8bbb0055c7c
1amdara <77836449+1amdara@users.noreply.github.com>
2022-03-10 05:23:38 -0800

    Add about text

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,5 +1,10 @@
 const request = require('request')
 
+
+
+
+
+
 const forecast = (latitude, longitude, callback) => {
      const url = 'http://api.weatherstack.com/current?access_key=6cf1b9100a1acbfcf3886e1b739e6e7c&query=' + latitude + ',' + longitude + '&units=f'
      
@@ -9,7 +14,8 @@ const forecast = (latitude, longitude, callback) => {
          } else if (body.error) {
                callback('Unable to find location', undefined)
          } else {
-               callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out. ')
+               console.log( body.current.weather_descriptions[0])
+               callback(undefined,body.current.weather_descriptions.summary + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out. ')
          }
     })
     }
